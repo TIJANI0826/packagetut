758ac4714af1293d6b4791013e95eab54c89ee47
TIJANI0826 <ibrahimtijani08@gmail.com>
2024-07-26 16:11:33 +0100

    First commit for packagetut

---
+++ main.py
@@ -0,0 +1,10 @@
+destroy_program = input("Type S to Start, E to exit the program")
+while destroy_program != "E":
+    username = input("Type in your username: ")
+    password = input("Type your password: ")
+
+    print(f"username {username}, password {password}")
+
+    destroy_program = input("Type S to Start/Continue, E to exit the program ")
+
+    
